970cd23a89f214338b0418e52e74fd666c75a1b1
Jeferson Siqueira <jeferson0306@msn.com>
2024-07-06 12:40:44 -0300

    Adicionando arquivo .csv gerado

--- convert-exam-data.py
+++ convert-exam-data.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from unidecode import unidecode
 
+
 # Função para remover acentos de uma string
 def remove_accents(input_str):
     return unidecode(input_str)
+
 
 # Carregar o arquivo Excel
 df = pd.read_excel('exame-csv.xlsx')
